93968c0af6d94e4480cb5d1a797b35ce7398dfab
Thirumalesh <kunti.thirumalesh@gmail.com>
2023-03-14 18:21:41 +0530

    created controller class for testing purpose

---
+++ src/main/java/com/spring/jenkins/demo/controller/JenkinsController.java
@@ -0,0 +1,18 @@
+package com.spring.jenkins.demo.controller;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+
+	private Logger logger = LoggerFactory.getLogger(JenkinsController.class);
+	@PostConstruct
+	public String getMessage() {
+		logger.info("welcome message in Controller class...");
+		return "Spring boot with jenkins integration..";
+	}
+}
