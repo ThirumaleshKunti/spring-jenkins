366107ef8190347cfecffb57ee4d9c24d4bbdb6c
Thirumalesh <kunti.thirumalesh@gmail.com>
2023-03-14 17:50:21 +0530

    first commit

---
+++ src/main/java/com/spring/jenkins/demo/SpringJenkinsDemoApplication.java
@@ -0,0 +1,24 @@
+package com.spring.jenkins.demo;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsDemoApplication {
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsDemoApplication.class);
+
+	@PostConstruct
+	public void init() {
+		logger.info("init methd execution...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Main method execution...");
+		SpringApplication.run(SpringJenkinsDemoApplication.class, args);
+	}
+
+}
